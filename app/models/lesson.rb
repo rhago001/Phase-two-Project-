77f3787992a6f10eee84588ef9f66d38a6c0b153
Rahel_Hagos <hagos.rahelh@gmail.com>
2021-01-08 12:59:54 -0500

    update

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,7 +6,7 @@ class Lesson < ApplicationRecord
    validates :date, presence: true 
    validates_format_of :date, presence: true, :with => /\d{2}\/\d{2}\/\d{4}/, :message => "must be in the following format: mm/dd/yyyy"
    # validates :date, numericality: { greater_than_or_equal_to: Date.today.year }
-   # validates :time, presence: true 
+   validates :time, presence: true 
    # validates :hand_position, :inclusion => { :in => %w(Fiction Non-Fiction) }
 
    def name
