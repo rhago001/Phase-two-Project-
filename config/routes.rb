c06a4ceb45f361c948f6555fbbe05e05c77dab8e
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-01-07 19:09:30 -0500

    update to lessons pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
  get 'signup', to: "users#new", as: 'signup' 
  get 'login', to: "sessions#new", as: 'login'
  post 'login', to: "sessions#create"
+
+
  delete 'logout', to: "sessions#destroy", as: 'logout'
 #  delete 'delete', to: "lessons#destroy", as: 'cancel'
 
